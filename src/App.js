fb6a38b112f91330db6c335da4b6ad404c4bd064
ciaranrdnr <ciaranurdenara@gmail.com>
2021-07-13 18:36:30 +0700

    added playlist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
+
 
 function App() {
-  console.log(process.env.REACT_APP_SPOTIFY_CLIENTID)
+  console.log(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div id="card-playlist">
+          <img id="track-img" src={data.album.images[0].url} alt="Album" width=""/>
+          <div >
+            <p id="track-title" className="detail">{data.name}</p>
+            <p id="track-artist" className="detail">{data.artists[0].name}</p>
+            <p id="track-album" className="detail">{data.album.name}</p>
+            <button id="track-button" className="detail"><div class="triangle-right"></div></button>
+        </div>
+      </div>
     </div>
   );
 }
